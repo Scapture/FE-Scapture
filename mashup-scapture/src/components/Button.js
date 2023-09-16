ca160acc7d2c4c7b60a19b9b09346bc023dbecdb
rtgc0915 <rtgc0915@gmail.com>
2023-09-17 01:44:05 +0900

    create-moverecoding components

--- mashup-scapture/src/components/Button.js
+++ mashup-scapture/src/components/Button.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const StyledButton = styled.button`
   /* 공통 스타일 */
   display: inline-flex;
   outline: none;
   border: none;
-  border-radius: 4px;
-  color: white;
-  font-weight: bold;
   cursor: pointer;
-  padding-left: 1rem;
-  padding-right: 1rem;
+  border-radius: 3.75rem;
+  background: var(--, #2458ea);
+  color: #F2F6FF;
+  font-family: Pretendard;
+  font-size: 1.5vw;
+  font-style: normal;
+  font-weight: 600;
+  justify-content:center;
+  align-items:center;
 
   /* 크기 */
-  height: 2.25rem;
-  font-size: 1rem;
+  width: 20vw;
+  height: 5vw;
+  flex-shrink: 0;
 
   /* 색상 */
-  background: #228be6;
+  background: #2458EA;
   &:hover {
     background: #339af0;
   }
--- mashup-scapture/src/components/Moverecording.js
+++ mashup-scapture/src/components/Moverecording.js
@@ -3,50 +3,63 @@ import styled from "styled-components";
 import { Link } from "react-router-dom"; // 라우팅을 위해 react-router-dom에서 Link 컴포넌트를 가져옴
 import Button from "./Button";
 
+const Container = styled.div`
+  margin-left: 18.4vw;
+`;
+
 const Title = styled.div`
-  color: var(--, #d6df22);
+  margin-top: 20vh;
+  color: #d6df22;
   font-family: Pretendard;
-  font-size: 1.25rem;
+  font-size: 1.5vw;
   font-style: normal;
   font-weight: 400;
   line-height: 1.25rem; /* 100% */
 `;
 
 const Titlecontent = styled.div`
+  margin-top: 0.5rem;
+  margin-bottom: 1rem;
   color: var(--1, #f2f6ff);
   font-family: Pretendard;
-  font-size: 2.5rem;
+  font-size: 2.5vw;
   font-style: normal;
   font-weight: 600;
-  line-height: 3.75rem; /* 150% */
+  line-height: 2.5rem;
+`;
+
+const Titleline = styled.div`
+  width: 26vw;
+  height: 0.25rem;
+  background: linear-gradient(90deg, #d6df22 0%, rgba(255, 255, 255, 0) 100%);
 `;
 
 const Titlesubcontent = styled.div`
+  margin-top: 1rem;
+  margin-bottom: 5vw;
+
   color: var(--1, #f2f6ff);
   font-family: Pretendard;
-  font-size: 1rem;
+  font-size: 1vw;
   font-style: normal;
   font-weight: 400;
-  line-height: 1.875rem; /* 187.5% */
+  line-height: 1rem;
 `;
 
 const AppBlock = styled.div`
-  width: 512px;
-  margin: 0 auto;
-  margin-top: 4rem;
-  border: 1px solid black;
-  padding: 1rem;
+  display: flex;
 `;
 
 function Moverecording() {
   return (
-    <>
+    <Container>
       <Title>RECORD</Title>
       <Titlecontent>
         지금 바로 당신의
         <br />
         운동 영상을 녹화해보세요!
       </Titlecontent>
+      <Titleline />
       <Titlesubcontent>
         SCAPTURE가 당신의 운동을 알아서 녹화하고 편집해줍니다!
         <br /> 당신만의 운동 영상을 녹화해보세요!
@@ -54,7 +67,7 @@ function Moverecording() {
       <AppBlock>
         <Button>녹화하기</Button>
       </AppBlock>
-    </>
+    </Container>
   );
 }
 
