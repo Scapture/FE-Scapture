94f5671e5716ddb7d6212aca2936d5a2fd09891a
rtgc0915 <rtgc0915@gmail.com>
2023-09-19 16:00:51 +0900

    matt uninstall for catch error

--- mashup-scapture/src/components/RecordingBtn.js
+++ mashup-scapture/src/components/RecordingBtn.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import mqtt from "mqtt"; // MQTT 라이브러리 추가
+//import mqtt from "mqtt"; // MQTT 라이브러리 추가
 
-
-function RecordingBtn() {
-  const StyledButton = styled.button`
+const StyledButton = styled.button`
     /* 공통 스타일 */
     display: inline-flex;
     outline: none;
@@ -39,22 +37,23 @@ function RecordingBtn() {
       margin-left: 1rem;
     }
   `;
-
   
+
+function RecordingBtn() {
   const [isRecording, setIsRecording] = useState(false);
   
   // TODO: MQTT 브로커 URL을 설정합니다.
-  const mqttClient = mqtt.connect("mqtt://your-mqtt-broker-url"); // MQTT 브로커 연결
+  //const mqttClient = mqtt.connect("mqtt://your-mqtt-broker-url"); // MQTT 브로커 연결
 
   const toggleRecording = () => {
     setIsRecording(!isRecording);
 
     // TODO: MQTT 토픽과 메시지 형식을 프로젝트에 맞게 수정필요.
-    if (mqttClient) {
-      const topic = "your-topic"; // MQTT 토픽 수정
-      const message = isRecording ? "녹화 종료" : "녹화 시작"; // 메시지 형식 수정
-      mqttClient.publish(topic, message);
-    }
+    // if (mqttClient) {
+    //   const topic = "your-topic"; // MQTT 토픽 수정
+    //   const message = isRecording ? "녹화 종료" : "녹화 시작"; // 메시지 형식 수정
+    //   mqttClient.publish(topic, message);
+    // }
   };
 
   return (
